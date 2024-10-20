58ebbc97fb2c7581ea885c437774eed72c448dae
karl2522 <153877407+karl2522@users.noreply.github.com>
2024-10-20 10:24:45 +0800

    Added appointments page
    added appointments

--- mainpages/mainpages/urls.py
+++ mainpages/mainpages/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from homepage import views    
+from homepage import views   
 
 urlpatterns = [
     path('admin/', admin.site.urls),
